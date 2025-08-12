ddd8f0884cad59daf3ef5145dc6d30226b267715
Zeliha İnan <zelihainann@gmail.com>
2025-08-12 16:34:03 +0300

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Zeliha İnan on 12.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
