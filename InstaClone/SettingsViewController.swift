fc47a8a426717891f3907b6e824ba7a7cfa111b7
Zeliha İnan <zelihainann@gmail.com>
2025-08-13 17:17:17 +0300

    Sign In & Sign Out functions completed

--- InstaClone/SettingsViewController.swift
+++ InstaClone/SettingsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SettingsViewController: UIViewController {
 
@@ -16,7 +17,11 @@ class SettingsViewController: UIViewController {
     
     
     @IBAction func logoutClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toViewController", sender: nil)
+        do {
+            try Auth.auth().signOut()
+            self.performSegue(withIdentifier: "toViewController", sender: nil)
+        } catch {
+            print("Failed to sign out: \(error.localizedDescription)")
+        }
     }
-    
 }
--- InstaClone/ViewController.swift
+++ InstaClone/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -15,18 +16,48 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     
     @IBAction func signInClicked(_ sender: Any) {
+            
+        if emailText.text != "" && passwordText.text != "" {
+            
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { (authdata, error) in
+            if error != nil {
+                self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+            }else {
+                self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+            }
+        }
+            
+        }else {
+            makeAlert(titleInput: "Error", messageInput: "Username or password cannot be empty")
+        }
         
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
     }
     
     
     @IBAction func signUpClicked(_ sender: Any) {
+        if emailText.text != "" && passwordText.text != "" {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { (authdata, error) in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                }else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+        }else{
+            makeAlert(titleInput: "Error", messageInput: "Username or password cannot be empty")
+        }
+                
     }
-    
+    func makeAlert (titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
